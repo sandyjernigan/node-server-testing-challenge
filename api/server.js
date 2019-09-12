24fd902656dd827d71dd050102de1b3eaac36152
Sandy <sandy.jernigan@gmail.com>
2019-09-11 23:07:50 -0400

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 // Routers
+const authRouter = require('./auth/auth-router.js');
 const usersRouter = require('./users/users-router.js');
 
 const server = express();
@@ -16,6 +17,7 @@ server.get('/', (req, res) => {
 });
 
 // Routes
+server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 module.exports = server;
